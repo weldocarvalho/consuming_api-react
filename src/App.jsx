a7dd02ecc2d60961a7f4f7f6d86045f4a3f0f979
weldocarvalho <weldocarvalho@outlook.com>
2021-09-20 17:37:11 -0300

    style: eslint, prettier and editorConfig add

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [students, setStudents] = useState([]);
+  const [students, setStudents] = useState([])
 
   useEffect(() => {
     async function getData() {
-      const response = await axios.get("http://localhost:3535/students");
-      const { students } = response.data
-      setStudents(students);
+      const response = await axios.get('http://localhost:3535/students')
+      const { students: studentsData } = response.data
+      setStudents(studentsData)
     }
 
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   return (
     <div className="App">
-      {students.map(student => (
-        <li>{student.name}</li>
+      {students.map((student) => (
+        <li key={student.id}>{student.name}</li>
       ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
